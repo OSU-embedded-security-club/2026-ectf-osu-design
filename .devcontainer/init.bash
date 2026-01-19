2ddec75dc34abcc79b78930536ba197190891aad
Mason Erwine <mason@erwine.dev>
2026-01-18 21:11:28 -0500

    Working Blink LED

---
+++ .devcontainer/init.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo dnf install -y wget curl git cmake clangd uv doxygen
+
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
+
+wget -O ti_llvm_installer.bin https://dr-download.ti.com/software-development/ide-configuration-compiler-or-debugger/MD-ayxs93eZNN/4.0.2.LTS/ti_cgt_armllvm_4.0.2.LTS_linux-x64_installer.bin
+chmod +x ti_llvm_installer.bin
+
+mkdir /ti
+./ti_llvm_installer.bin --mode unattended --prefix /ti
+
+echo 'export TI_LLVM_ROOT=/ti/ti-cgt-armllvm_4.0.2.LTS' >> /etc/bashrc
+echo 'export PATH=$PATH:/ti/ti-cgt-armllvm_4.0.2.LTS/bin' >> /etc/bashrc
--- src/main.c
+++ src/main.c
@@ -1,7 +1,36 @@
 #include "sysconfig/ti_msp_dl_config.h"
 
-int main() {
+
+//! Program Entrypoint
+int main(void) {
+    
+    // Configure System
     SYSCFG_DL_init();
 
-    return 0;
+    // Setup LED pin
+    DL_GPIO_clearPins(GPIOB, DL_GPIO_PIN_14);
+    DL_GPIO_enableOutput(GPIOB, DL_GPIO_PIN_14);
+
+    char msg[] = "Initalized\n";
+    for(int i = 0; i < sizeof(msg); i++) {
+        DL_UART_transmitDataBlocking(HOST_INST, msg[i]);
+    }
+
+    // Blink LED
+    while(true) {
+        DL_GPIO_togglePins(GPIOB, DL_GPIO_PIN_14);
+
+        delay_cycles(10000 * 32000);
+    }
+
+}
+
+// This doesn't work yet
+void HOST_INST_IRQHandler(void) {
+    uint8_t buf[4];
+    DL_UART_drainRXFIFO(HOST_INST, buf, sizeof(buf));
+
+    for(int i = 0; i < sizeof(buf); i++) {
+        DL_UART_transmitDataBlocking(HOST_INST, buf[i]);
+    }
 }
