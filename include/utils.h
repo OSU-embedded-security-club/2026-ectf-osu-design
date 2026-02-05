b93c53aa534ca624a0da543c05471888c713372a
HeronErin <HeronErin@protonmail.com>
2026-02-05 10:25:49 -0500

    Add back in debug function

--- include/utils.h
+++ include/utils.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "ti_msp_dl_config.h"
 
@@ -19,3 +22,25 @@ bool utils_verify_pin(uint8_t* pin, size_t pin_length);
  * @param length    Buffer Length
  */
 void utils_send_buffer(UART_Regs * uart, void* buffer, size_t length);
+
+
+
+#ifdef DEBUG
+#define _WRITE_TO(FUNC, ...) do{                            \
+  char* _buffer;                                            \
+  if (-1 != asprintf(&_buffer, __VA_ARGS__)){ \
+    FUNC(HOST_INST, _buffer, strlen(_buffer)); \
+    free(_buffer); \
+  } \
+} while(0);
+
+#define print_debug(...) _WRITE_TO(message_header_send_debug, __VA_ARGS__)
+#define print_error(...) _WRITE_TO(message_header_send_error, __VA_ARGS__)
+
+
+#else
+
+#define print_debug(...) {}
+#define print_error(...)  message_header_send_error(HOST_INST, 0, 0)
+
+#endif
