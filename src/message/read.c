8e9fbbd2cf161b77f7be28d045a20259d640242a
Mattwike <matthewdweikel@gmail.com>
2026-02-04 19:22:06 -0500

    outline for read

--- src/message/read.c
+++ src/message/read.c
@@ -1,7 +1,15 @@
 #include "message/read.h"
+#include <monocypher.h>
 
 void message_read_response(message_header_t header) {
     // TODO: Implement read response
+    //pull entire file into memory
+    //check to see if the group has the private read key
+    //if it has the private key key exchange with the metadata write pub key
+    //use crypto_x25519() 
+    //else return an erorr
+    //decrypt with the key and delete it from memory using crypto_wipe()
+    //check the last block with the aad and if not the same return error, else return data
 
     char msg[] = "Read not implemented yet";
     message_header_send_error(HOST_INST, msg, sizeof(msg));
