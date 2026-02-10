3cf46a23291d2efd8ca92ff2c95e6a59ad400cd7
HeronErin <HeronErin@protonmail.com>
2026-02-10 10:34:35 -0500

    spaces

--- generate_secrets.py
+++ generate_secrets.py
@@ -3,26 +3,18 @@
 import os, struct, hashlib, time
 from os.path import join, dirname, abspath
 
-
-
 SOURCE_DIR = dirname(abspath(__file__))
-SECRETS_BASE = join(
-    SOURCE_DIR,
-    "include", "secrets.h.base"
-)
-SECRETS_OUT = join(
-    SOURCE_DIR,
-    "include", "secrets.h"
-) 
+SECRETS_BASE = join(SOURCE_DIR, "include", "secrets.h.base")
+SECRETS_OUT = join(SOURCE_DIR, "include", "secrets.h")
 
-# Contants 
+# Contants
 
 # **THIS IS WHERE YOU TUNE THE HASHING TIME**:
 
 # 64 byte blake2b = 155503 cycles, or 0.00485946875 seconds, or 205.78hz
-# Here, we are targeting 250 ms for the STAGE1 hash. 
+# Here, we are targeting 250 ms for the STAGE1 hash.
 # Please note, this is a hilariously weak hash, but it is the best we can do.
-STAGE1_PIN_ITERATIONS =  int((205 / 1000) * 250)
+STAGE1_PIN_ITERATIONS = int((205 / 1000) * 250)
 
 
 STAGE1_KEY = os.urandom(32)
@@ -34,10 +26,9 @@ STAGE2_RAND_INT = struct.unpack("<I", os.urandom(4))[0]
 PIN = "aBcDe6"
 
 
-
 def hash_pin(pin):
     assert len(pin) == 6
-    # Throws an error if invalid 
+    # Throws an error if invalid
     int(pin, 16)
 
     hash = pin.lower().encode("ascii") + STAGE1_PADDING
@@ -47,14 +38,11 @@ def hash_pin(pin):
 
 
 def make_c_bytes(data):
-    return  "{" + ", ".join(f"0x{b:02x}" for b in data) + "}"
-
+    return "{" + ", ".join(f"0x{b:02x}" for b in data) + "}"
 
 
 def main():
     base = open(SECRETS_BASE).read()
-    
-    
 
     f = open(SECRETS_OUT, "w")
     f.write(base)
@@ -65,30 +53,5 @@ def main():
     f.write(f"\n\n#define STAGE1_PIN_HASH {make_c_bytes(hash_pin(PIN))}")
 
 
-
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
