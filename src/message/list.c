71621f31e933428f53c34d1b2153307348986f8e
McBrosum <*&FeyUFHEn87tyb7F*_&Ef9[HE[9fmy&ENf87MSDY(fm8YES*&FtyN*)E(yfm98E@example.com>
2026-02-10 08:09:28 -0500

    Updated pseudocode.

--- src/message/list.c
+++ src/message/list.c
@@ -1,5 +1,7 @@
 #include "message/list.h"
 
+#define MAX_RETRY_DELAY_MS 5000
+
 void message_list_response(message_header_t header) {
     // TODO: Implement list response
 
@@ -16,30 +18,32 @@ void message_list_response(message_header_t header) {
     uvx ectf tools COM10 list 123456
     Options:
     --help
-
     */
 
     /*
     Vulnerabilities to keep in mind:
     1. Timing attacks when inserting pins
-    2. Brute force
+    2. Brute force (only 1,000,000 combinations)
     3. Buffer overflows - deals with file names
     4. 
 
     Solutions for vulnerabilities
     1. Retry counter
-    2. 
-    3. 
-    4. 
+    2. Brute force delay (5 seconds)
+    3. Constant time check for pin
+    4. Make the pin verification inline
     */
 
     /*
-    psuedocode
-    get the PIN from the user
-    hash the entered pin and check it with the stored pin hash
-    If the hash works, then get the file names stored on the board
-    List all the files on the system
-    if the hash does not work, then return an error
+    psuedocode:
+    get the pin from the user
+    hash and salt the pin
+    inline check the pin in constant time
+    If correct
+        list all files on the system
+    else
+        deny and wait 5 seconds
+    clear sensitive data
     */
 
     char msg[] = "List not implemented yet";
