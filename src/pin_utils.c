bd715a017bd88e37d5a9eab4bdbf2b3447c3e160
Andrew Langan <alangan444@icloud.com>
2026-02-26 02:22:05 +0000

    Fixed constant-time compare and cleaned up pin_utils

--- src/pin_utils.c
+++ src/pin_utils.c
@@ -1,58 +1,61 @@
 #include "pin_utils.h"
-#include <ctype.h>
-#include <string.h>
-#include <monocypher.h>
+#include "rng.h"
 #include "secrets.h"
+#include <ctype.h>
+#include <monocypher.h>
 #include <stdalign.h>
-#include "random.h"
-
-
-
+#include <string.h>
 
 static uint8_t pin_hash[] = STAGE1_PIN_HASH;
 static const uint8_t stage1_key[] = STAGE1_KEY;
 
-// Changed each reboot (to fuck with attackers)
-static alignas(int) uint8_t random_data[32];
-static uint8_t stage2_key[32];
+// Changed each reboot
+static alignas(int) uint8_t random_data[32] = {0};
+static uint8_t stage2_key[32] = {0};
 
-#define SEED ( *(int*)&random_data[4*__COUNTER__] )
+#define SEED (*(int *)&random_data[4 * __COUNTER__])
 static void apply_stage2(uint8_t *hash);
 
 // A potential option for something read as a secret,
-// reading the data here intentionally leads to 
+// reading the data here intentionally leads to
 // deterministic undefined behavior.
-static void _unused(){}
+static void _unused(void) {}
 
+/**
+ * @brief Applies the first stage of the pin hashing process, which consists of
+ * iteratively hashing the input pin hash with a fixed key for a set number of
+ * iterations.
+ *
+ * @param[inout] hash Input hash to apply stage 1 to, which will be modified in
+ * place to contain the result of stage 1. Must be 64 bytes long.
+ */
+static void apply_stage1(uint8_t *const hash) {
+  uint8_t temp[64] = {0};
 
-// Hash MUST be 64 long
-static void apply_stage1(uint8_t *hash)
-{
-  uint8_t temp[64];
-
-  for (int i = 0; i < STAGE1_PIN_ITERATIONS; i++) {
-    crypto_blake2b_keyed(
-        temp,
-        64,
-        stage1_key,
-        sizeof(stage1_key),
-        hash,
-        64);
+  for (size_t i = 0; i < STAGE1_PIN_ITERATIONS; ++i) {
+    crypto_blake2b_keyed(temp, 64, stage1_key, sizeof(stage1_key), hash, 64);
     memcpy(hash, temp, 64);
   }
   crypto_wipe(temp, 64);
 }
 
-
-
+/**
+ * @brief Validates a provided pin by applying the hashing process and comparing
+ * the result to the expected hash. The pin is expected to be a 6-character
+ * hexadecimal string.
+ *
+ * @param pin Null-terminated string containing the pin to validate. Must be 6
+ * characters long and consist of hexadecimal digits (0-9, A-F, a-f).
+ * @return true if the provided pin is valid, false otherwise
+ */
 bool validate_pin(const char pin[6]) {
-  uint8_t hash[64];
+  uint8_t hash[64] = {0};
 
 #pragma unroll
-  for (int i = 0; i < 6; i++) {
+  for (size_t i = 0; i < 6; ++i) {
     if ((pin[i] >= '0' && pin[i] <= '9') || (pin[i] >= 'A' && pin[i] <= 'F') ||
         (pin[i] >= 'a' && pin[i] <= 'f')) {
-      hash[i] = (char)tolower(pin[i]);
+      hash[i] = (uint8_t)tolower(pin[i]);
     } else {
       return false;
     }
@@ -64,60 +67,67 @@ bool validate_pin(const char pin[6]) {
   apply_stage1(hash);
   apply_stage2(hash);
 
-  // Constant time memcmp
-  volatile bool is_valid = true;
-#pragma unroll
-  for (int i = 0; i < 64; i++)
-    is_valid &= hash[i] == pin_hash[i];
+  const bool is_valid = crypto_verify64(hash, pin_hash);
 
   crypto_wipe(hash, sizeof(hash));
   return is_valid;
 }
 
-void init_pin() {
-  random_fill_buffer(random_data, sizeof(random_data));
-  random_fill_buffer(stage2_key, sizeof(stage2_key));
+/**
+ * @brief Initializes the pin hashing process by generating random data,
+ * applying stage 2 to the stage 1 hash, and preparing any necessary data for
+ * the pin validation process.
+ *
+ */
+void init_pin(void) {
+  rng_get_bytes(random_data, sizeof(random_data));
+  rng_get_bytes(stage2_key, sizeof(stage2_key));
 
   apply_stage2(pin_hash);
 }
 
-#define COMPILER_FUNC_OPTS ((uint8_t*[]) {\
-   (uint8_t*) &validate_pin,     \
-   (uint8_t*) &apply_stage1,     \
-   (uint8_t*) &apply_stage2,     \
-   (uint8_t*) &_unused,     \
-})
-#define RANDOM_FUNC COMPILER_FUNC_OPTS[SEED % (sizeof(COMPILER_FUNC_OPTS) / sizeof(void*))]
+#define COMPILER_FUNC_OPTS                                                     \
+  ((uint8_t *[]){                                                              \
+      (uint8_t *)&validate_pin,                                                \
+      (uint8_t *)&apply_stage1,                                                \
+      (uint8_t *)&apply_stage2,                                                \
+      (uint8_t *)&_unused,                                                     \
+  })
+#define RANDOM_FUNC                                                            \
+  COMPILER_FUNC_OPTS[SEED % (sizeof(COMPILER_FUNC_OPTS) / sizeof(void *))]
 
-// Hash MUST be 64 long
-static void apply_stage2(uint8_t *hash) {
-  uint8_t temp[64];
+/**
+ * @brief Applies the second stage of the pin hashing process, which consists of
+ * hashing the input hash with a key derived from the stage 2 key and random
+ * data.
+ *
+ * @param[inout] hash Input hash to apply stage 2 to. Must be 64 bytes long.
+ */
+static void apply_stage2(uint8_t *const hash) {
+  uint8_t temp[64] = {0};
 
-  crypto_blake2b_ctx ctx;
+  crypto_blake2b_ctx ctx = {0};
   crypto_blake2b_keyed_init(&ctx, 64, stage2_key, sizeof(stage2_key));
 
   crypto_blake2b_update(&ctx, hash, 64);
 
-  int int_data[] = {
-    FACTORYREGION->BOOTCRC, // Crc of ALL board specific values
-    (int) (size_t) RANDOM_FUNC, // Location in memory
-    STAGE2_RAND_INT, // Random int from generate_secrets.
+  const int int_data[3] = {
+      (int)FACTORYREGION->BOOTCRC, // Crc of ALL board specific values
+      (int)RANDOM_FUNC,            // Location in memory
+      STAGE2_RAND_INT,             // Random int from generate_secrets.
   };
-  crypto_blake2b_update(&ctx, (uint8_t*) int_data, sizeof(int_data));
-  
+  crypto_blake2b_update(&ctx, (const uint8_t *)int_data, sizeof(int_data));
+
   // Different each reboot
   crypto_blake2b_update(&ctx, random_data, sizeof(random_data));
 
   // Part of the bootloader
-  crypto_blake2b_update(&ctx, (uint8_t*) 0, 64);
+  crypto_blake2b_update(&ctx, (uint8_t *)0, 64);
 
   // Compiler rng
   crypto_blake2b_update(&ctx, RANDOM_FUNC, 64);
-
 
   crypto_blake2b_final(&ctx, temp);
   memcpy(hash, temp, 64);
   crypto_wipe(temp, 64);
 }
-
-
