79b20d588bae6fb7691e55bbbe617dfbf0d4fe56
Andrew Langan <alangan444@icloud.com>
2026-02-26 03:07:09 +0000

    Cleaned up file

--- include/message/header.h
+++ include/message/header.h
@@ -1,54 +1,53 @@
 #pragma once
 
-#include "../sysconfig/ti_msp_dl_config.h"
+#include "ti_msp_dl_config.h"
 
-typedef enum {
-    //! List files command/response
-    MESSAGE_LIST = 'L',
-    //! Read file command/response
-    MESSAGE_READ = 'R',
-    //! Write file command/response
-    MESSAGE_WRITE = 'W',
-    //! Receive file command/response
-    MESSAGE_RECEIVE = 'C',
-    //! Interrogate files command/response
-    MESSAGE_INTERROGATE = 'I',
-    //! Listen command/response
-    MESSAGE_LISTEN = 'N',
-    //! Acknowledge the receipt of data
-    MESSAGE_ACK = 'A',
-    //! Notify of an error/failure. Exits Host Tool
-    MESSAGE_ERROR = 'E',
-    //! Provide debug information (ignored by testing framework)
-    MESSAGE_DEBUG = 'D',
+typedef enum : char {
+  //! List files command/response
+  MESSAGE_LIST = 'L',
+  //! Read file command/response
+  MESSAGE_READ = 'R',
+  //! Write file command/response
+  MESSAGE_WRITE = 'W',
+  //! Receive file command/response
+  MESSAGE_RECEIVE = 'C',
+  //! Interrogate files command/response
+  MESSAGE_INTERROGATE = 'I',
+  //! Listen command/response
+  MESSAGE_LISTEN = 'N',
+  //! Acknowledge the receipt of data
+  MESSAGE_ACK = 'A',
+  //! Notify of an error/failure. Exits Host Tool
+  MESSAGE_ERROR = 'E',
+  //! Provide debug information (ignored by testing framework)
+  MESSAGE_DEBUG = 'D',
 } message_operation_t;
-
 
 /**
  * @brief Message Initalization Packet
- * 
+ *
  * @note All messages should begin with '%'.
  */
-typedef struct {
-    message_operation_t operation;
-    uint16_t message_length;
+typedef struct __PACKED {
+  message_operation_t operation;
+  uint16_t message_length;
 } message_header_t;
 
 /**
  * @brief Parses Message Header from uart
  *
  * @param[in] Uart Registers to parse header from
- * 
+ *
  * @returns Request Headers
  */
-message_header_t message_header_request(UART_Regs *uart);
+message_header_t message_header_request(UART_Regs *const uart);
 
 /**
  * @brief Starts a Response to the Host Tools
- * 
+ *
  * @param[in] ctx   Message Header
  */
-void message_header_response(UART_Regs* uart, message_header_t ctx);
+void message_header_response(UART_Regs *const uart, const message_header_t ctx);
 
 /**
  * @brief Sends an error message to the Host Tools
@@ -57,7 +56,8 @@ void message_header_response(UART_Regs* uart, message_header_t ctx);
  * @param[in] msg       Error message string
  * @param[in] msg_len   Length of the error message
  */
-void message_header_send_error(UART_Regs* uart, const char* msg, size_t msg_len);
+void message_header_send_error(UART_Regs *const uart, const char *const msg,
+                               const uint16_t msg_len);
 
 /**
  * @brief Sends a debug message to the Host Tools
@@ -66,11 +66,12 @@ void message_header_send_error(UART_Regs* uart, const char* msg, size_t msg_len)
  * @param[in] msg       Debug message string
  * @param[in] msg_len   Length of the debug message
  */
-void message_header_send_debug(UART_Regs* uart, const char* msg, size_t msg_len);
+void message_header_send_debug(UART_Regs *const uart, const char *const msg,
+                               const uint16_t msg_len);
 
 /**
  * @brief Sends an ACK message to the Host Tools
  *
  * @param[in] uart      UART registers for communication
  */
-void message_header_send_ack(UART_Regs* uart);
+void message_header_send_ack(UART_Regs *const uart);
