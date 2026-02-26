0c3655e80b7e10e2731782a98ff544d48a72e25b
Andrew Langan <alangan444@icloud.com>
2026-02-26 03:06:52 +0000

    Added more utilities to simplify packet tx/rx

--- include/utils.h
+++ include/utils.h
@@ -1,57 +1,125 @@
 #pragma once
-#include <string.h>
+
+#include "message/header.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "message/header.h"
+#include <string.h>
 
 #include "ti_msp_dl_config.h"
 
 /**
+ * @brief Macro to indicate that a function parameter must not be NULL. If a
+ * NULL pointer is passed to a parameter marked with this attribute, gcc will
+ * emit a warning at compile time.
+ *
+ */
+#define NOTNULL() __attribute__((nonnull))
+
+/**
  * @brief Parses and Verifies Host Pin
- * 
+ *
  * @return true     Host provided a valid pin
  * @return false    Host provided an invalid pin
  */
-bool utils_verify_pin(uint8_t* pin, size_t pin_length);
-
+NOTNULL()
+bool utils_verify_pin(const uint8_t *const pin, const size_t pin_length);
 
 /**
  * @brief Sends a Buffer over UART
- * 
- * @param uart      UART to send Buffer Over
- * @param buffer    Buffer Pointer
- * @param length    Buffer Length
+ *
+ * @param[in] uart      UART to send Buffer Over
+ * @param[in] buffer    Buffer Pointer
+ * @param[in] length    Buffer Length
  */
-void utils_send_buffer(UART_Regs * uart, const void* buffer, size_t length);
+NOTNULL()
+void utils_send_bytes(UART_Regs *const uart, const void *const buffer,
+                      const size_t length);
 
 /**
  * @brief Receives a specified number of bytes from the UART
  *
- * Blocks until `num_bytes` have been read from the provided `uart`
+ * Blocks until `length` have been read from the provided `uart`
  * and stored into `buffer`.
  *
- * @param uart      UART peripheral to read from
- * @param buffer    Destination buffer where received bytes will be written
- * @param num_bytes Number of bytes to receive
+ * @param[in] uart      UART peripheral to read from
+ * @param[out] buffer    Destination buffer where received bytes will be written
+ * @param[in] length Number of bytes to receive
  */
-void utils_receive_bytes(UART_Regs* uart, void* buffer, size_t num_bytes);
+NOTNULL()
+void utils_receive_bytes(UART_Regs *const uart, void *const buffer,
+                         const size_t length);
+
+/**
+ * @brief Sends a packet over UART, including the message header and payload.
+ *
+ * @param[in] uart UART peripheral to send buffer through
+ * @param[in] msg_type Message type (operation) to include in the header
+ * @param[in] buffer Pointer to the buffer payload to send
+ * @param[in] length Length of the buffer payload
+ */
+NOTNULL()
+void utils_send_packet(UART_Regs *const uart, const uint8_t msg_type,
+                       const void *const buffer, const uint16_t length);
+
+/**
+ * @brief Receives a packet from the UART, including the message header and
+ * payload.
+ *
+ * @param[in] uart UART registers to receive data from
+ * @param[out] buffer Buffer to store the received packet (must be large enough
+ * to hold header + payload)
+ * @param[out] length Pointer to a size_t variable where the total size of
+ * the received packet will be stored
+ */
+NOTNULL()
+void utils_receive_packet(UART_Regs *const uart, void *const buffer,
+                          size_t *const length);
+
+/**
+ * @brief Sends an ACK packet over UART
+ *
+ * @param[in] uart UART peripheral to send ACK through
+ */
+NOTNULL() void utils_send_ack(UART_Regs *const uart);
+/**
+ * @brief Reads an ACK packet from UART and verifies it is a valid ACK response
+ * @param[in] uart UART peripheral to read ACK from
+ * @return true if a valid ACK packet is received, false otherwise
+ *
+ */
+NOTNULL() bool utils_read_ack(UART_Regs *const uart);
+
+/**
+ * @brief Receives a specified number of bytes from the UART, sending an ACK
+ * after every 256 bytes to ensure flow control.
+ *
+ * @param[in] uart UART peripheral to receive data from
+ * @param[out] buffer Buffer to store received data (must be large enough to
+ * hold length)
+ * @param[in] length Number of bytes to receive
+ */
+NOTNULL()
+void utils_receive_bytes_and_ack(UART_Regs *const uart, void *const buffer,
+                                 const size_t length);
 
 #ifdef DEBUG
-#define _WRITE_TO(FUNC, ...) do{                            \
-  char* _buffer;                                            \
-  if (-1 != asprintf(&_buffer, __VA_ARGS__)){ \
-    FUNC(HOST_INST, _buffer, strlen(_buffer)); \
-    free(_buffer); \
-  } \
-} while(0);
+#define _WRITE_TO(FUNC, ...)                                                   \
+  do {                                                                         \
+    char *_buffer;                                                             \
+    if (-1 != asprintf(&_buffer, __VA_ARGS__)) {                               \
+      FUNC(HOST_INST, _buffer, strlen(_buffer));                               \
+      free(_buffer);                                                           \
+    }                                                                          \
+  } while (0);
 
 #define print_debug(...) _WRITE_TO(message_header_send_debug, __VA_ARGS__)
 #define print_error(...) _WRITE_TO(message_header_send_error, __VA_ARGS__)
 
-
 #else
 
-#define print_debug(...) {}
-#define print_error(...)  message_header_send_error(HOST_INST, 0, 0)
+#define print_debug(...)                                                       \
+  {                                                                            \
+  }
+#define print_error(...) message_header_send_error(HOST_INST, 0, 0)
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,21 +1,124 @@
 #include "utils.h"
+#include "message/header.h"
 #include "pin_utils.h"
+#include "ti_msp_dl_config.h"
 
-bool utils_verify_pin(uint8_t* pin, size_t pin_length) {
-  if (pin_length != 6) return false;
-  return validate_pin((const char*) pin);
+bool utils_verify_pin(const uint8_t *const pin, const size_t pin_length) {
+  if (pin_length != 6) {
+    return false;
+  }
+  return validate_pin((const char *)pin);
 }
 
-void utils_send_buffer(UART_Regs * uart, const void* buffer, size_t length) {
-    uint8_t * bytes = (uint8_t*) buffer;
+void utils_send_bytes(UART_Regs *const uart, const void *const buffer,
+                      const size_t length) {
+  const uint8_t *const temp = (const uint8_t *)buffer;
 
-    for(int i = 0; i < length; i++) {
-        DL_UART_transmitDataBlocking(uart, bytes[i]);
+  for (size_t i = 0; i < length; ++i) {
+    DL_UART_transmitDataBlocking(uart, temp[i]);
+  }
+}
+
+void utils_receive_bytes(UART_Regs *const uart, void *const buffer,
+                         const size_t length) {
+  uint8_t *const temp = (uint8_t *)buffer;
+
+  for (size_t i = 0; i < length; ++i) {
+    temp[i] = DL_UART_receiveDataBlocking(uart);
+  }
+}
+
+void utils_send_ack(UART_Regs *const uart) {
+  utils_send_packet(uart, MESSAGE_ACK, "", 0);
+}
+
+bool utils_read_ack(UART_Regs *const uart) {
+  const message_header_t header = message_header_request(uart);
+
+  return (header.operation == MESSAGE_ACK) && (header.message_length == 0);
+}
+
+void utils_receive_bytes_and_ack(UART_Regs *const uart, void *const buffer,
+                                 const size_t length) {
+  uint8_t *const temp = (uint8_t *)buffer;
+
+  for (size_t i = 0; i < length; ++i) {
+    const uint32_t rem = (length - i) > 256 ? 256 : (length - i);
+
+    if (((i % 256) == 0) && (i != 0)) {
+      utils_send_ack(uart);
     }
+
+    utils_receive_bytes(uart, &temp[i], rem);
+  }
 }
 
-void utils_receive_bytes(UART_Regs* uart, void* buffer, size_t num_bytes) {
-    uint8_t* temp = buffer;
-    for(int i = 0; i < num_bytes; i++)
-        temp[i] = DL_UART_receiveDataBlocking(HOST_INST);
+void utils_send_bytes_and_ack(UART_Regs *const uart, const void *const buffer,
+                              const size_t length) {
+  const uint8_t *const temp = (const uint8_t *)buffer;
+
+  for (size_t i = 0; i < length; ++i) {
+    const uint32_t rem = (length - i) > 256 ? 256 : (length - i);
+
+    if (((i % 256) == 0) && (i != 0)) {
+      utils_read_ack(uart);
+    }
+
+    utils_send_bytes(uart, &temp[i], rem);
+  }
+
+  fflush(stdout);
+}
+
+void utils_receive_packet(UART_Regs *const uart, void *const buffer,
+                          size_t *const length) {
+  uint8_t *const temp = (uint8_t *)buffer;
+
+  utils_receive_bytes(uart, temp, 1);
+  if (temp[0] != '%') {
+    *length = 0;
+    return;
+  }
+
+  const message_header_t header = message_header_request(uart);
+
+  memcpy(temp, &header, sizeof(header));
+
+  *length = header.message_length;
+
+  if ((header.operation != MESSAGE_ACK) &&
+      (header.operation != MESSAGE_ERROR) &&
+      (header.operation != MESSAGE_DEBUG)) {
+    utils_send_ack(uart);
+
+    if (header.message_length > 0) {
+      utils_receive_bytes_and_ack(uart, &temp[sizeof(header) + 1],
+                                  header.message_length);
+      utils_send_ack(uart);
+    }
+  }
+}
+
+void utils_send_packet(UART_Regs *const uart, const uint8_t msg_type,
+                       const void *const buffer, const uint16_t length) {
+  const char magic = '%';
+
+  utils_send_bytes(uart, &magic, sizeof(magic));
+
+  const message_header_t header = {.operation = msg_type,
+                                   .message_length = length};
+
+  utils_send_bytes(uart, &header, sizeof(header));
+
+  if ((msg_type == MESSAGE_ACK) || (msg_type == MESSAGE_ERROR) ||
+      (msg_type == MESSAGE_DEBUG)) {
+    return;
+  }
+
+  utils_read_ack(uart);
+
+  if (length > 0) {
+    utils_send_bytes_and_ack(uart, buffer, length);
+    utils_send_ack(uart);
+  }
 }
