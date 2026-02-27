9780aec52d4a373e1f98f447c6a718b365448c5a
Mason Erwine <mason@erwine.dev>
2026-02-27 20:54:56 +0000

    clone repos manually

--- firmware/build.sh
+++ firmware/build.sh
@@ -4,6 +4,9 @@ source /etc/bashrc
 
 cd /hsm
 
+git clone https://github.com/LoupVaillant/Monocypher.git
+git clone https://github.com/TexasInstruments/mspm0-sdk.git
+
 python3 generate_secrets.py /secrets/global.secrets ${HSM_PIN} "${PERMISSIONS}"
 
 cd /out
